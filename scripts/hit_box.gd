3b456090bf54bf5b717b89dced9cd69665f51ce8
robbyj1388 <johnsonrobby1388@gmail.com>
2026-08-15 23:23:18 -0400

    feat/stats-system:   - Added hitbox layout TO-DO:   - Still need to add a hitbox duration timer for the players cart bash

--- scripts/hit_box.gd
+++ scripts/hit_box.gd
@@ -1,8 +1,36 @@
-extends Area2D
-class_name HitBox
+class_name HitBox extends Area2D
+
 # Send a detection/overlap of a hurtbox to say
 # hey we're touching take X amount of damage (decided by parent node)
 
+var attacker_stats: Stats
+var hitbox_lifetime: float # TO-DO: Use for player cart charge
+var shape: Shape2D
+# TO-DO: add hitbox logging
+
+func _init(_attacker_stats: Stats, _hitbox_lifetime: float, _shape: Shape2D) -> void:
+	attacker_stats = _attacker_stats
+	hitbox_lifetime = _hitbox_lifetime
+	shape = _shape
+
+func _ready() -> void:
+	monitorable = false
+	area_entered.connect(_on_area_entered)
+	
+	if shape:
+		var collision_shape = CollisionObject2D
+		collision_shape.shape = shape
+		add_child(collision_shape)
+		
+	# Disable default layer/mask detection
+	set_collision_layer_value(1, false)
+	set_collision_mask_value(1, false)
+	match attacker_stats.faction:
+		Stats.Faction.PLAYER:
+			set_collision_mask_value(3, true)
+		Stats.Faction.ENEMY_SHOPPER:
+			set_collision_mask_value(2, true)
+
 func _on_area_entered(area: Area2D):
 	if area is HurtBox:
-		area.get_damage(get_parent().damage_to_deal)
+		area.get_damage(attacker_stats.current_damage)
