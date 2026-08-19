432b42d62b69a045d2b555eae6fa6b0c6ad77ab0
jayhwkns <hawkinsjr27@gmail.com>
2026-08-18 20:48:26 -0400

    Level infrastructure

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,15 +1,24 @@
+class_name GameManager
 extends Node
 
-var rng = RandomNumberGenerator.new()
-@export var game_seed: String = "SEED"
+static var _instance: GameManager
 
-@export var level_node: PackedScene
+@export var game_seed: String = "SEED"
+@export var level: Level
+@export var level_size: int = 16
+
+var rng = RandomNumberGenerator.new()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	rng.seed = game_seed.hash()
-	spawn_node()
+	_instance = self
+	setup_level()
 
-func spawn_node() -> void:
-	var instance = level_node.instantiate()
-	get_tree().root.add_child.call_deferred(instance)
+func setup_level():
+	for module in level.generate(rng, level_size):
+		get_tree().root.add_child.call_deferred(module)
+
+static func get_game_manager() -> GameManager:
+	return _instance
--- scripts/resources/item_catalog.gd
+++ scripts/resources/item_catalog.gd
@@ -1,10 +1,6 @@
 class_name ItemCatalog
 extends Resource
 
-@export var global_items: Array[Item] = []
-@export var floormart_items: Array[Item] = []
-@export var bestguy_items: Array[Item] = []
-@export var homerepo_items: Array[Item] = []
 @export var recipes: Array[Recipe] = []
 
 func recipes_containing(item: Item) -> Array[Recipe]:
