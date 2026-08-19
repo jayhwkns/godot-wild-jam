183279b1192a46723aa10d7cb3538b0cdd9d356c
jayhwkns <hawkinsjr27@gmail.com>
2026-08-18 21:01:03 -0400

    Support multiple levels

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -7,16 +7,17 @@ const player_scn = preload("res://scenes/player_controller.tscn")
 
 @export_group("Level Generation")
 @export var game_seed: String = "SEED"
-@export var level: Level
+@export var levels: Array[Level]
 @export var level_size: int = 16
 
 @export_group("Player")
 @export var place_at: Vector2
 
-
 var rng = RandomNumberGenerator.new()
 
 var player: PlayerController
+
+var level: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -32,7 +33,7 @@ func place_player():
 	get_tree().root.add_child.call_deferred(player)
 
 func setup_level():
-	for module in level.generate(rng, level_size):
+	for module in levels[level].generate(rng, level_size):
 		get_tree().root.add_child.call_deferred(module)
 
 static func get_game_manager() -> GameManager:
