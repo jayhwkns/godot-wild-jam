48f14535b84108740fd50de52ba611f353df511a
robbyj1388 <johnsonrobby1388@gmail.com>
2026-08-21 22:11:00 -0400

    feat/background:   - Randomizing time between spawning items

--- scripts/item_spawner.gd
+++ scripts/item_spawner.gd
@@ -1,13 +1,15 @@
 extends Node2D
+@onready var timer: Timer = $Timer
 
 @export var items: Array[Item]
 @export var world_item_scene: PackedScene 
 
 func _on_timer_timeout() -> void:
+	timer.wait_time = randi_range(1, 6)
 	if items.is_empty() or not world_item_scene:
 		print("Item spawner error")
 		return
-		
+
 	var item_resource = items.pick_random()
 	
 	var physical_item = world_item_scene.instantiate()
